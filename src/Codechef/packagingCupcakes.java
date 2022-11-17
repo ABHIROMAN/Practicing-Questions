4ff64862d042204237a9a245d7be5289233515ff
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-17 19:47:31 +0530

    updating the packaging cupcakes

---
+++ src/Codechef/packagingCupcakes.java
@@ -0,0 +1,20 @@
+package Codechef;
+
+import java.util.*;
+
+public class packagingCupcakes {
+
+    public static void main (String[] args) throws java.lang.Exception	{
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while(T --> 0) {
+
+            int N = sc.nextInt();
+
+            System.out.println((N/2) + 1);
+        }
+    }
+}
