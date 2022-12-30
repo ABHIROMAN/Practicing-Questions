1c1bb6a143cabc380454069a1b8835fb5d2071ea
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-30 21:47:47 +0530

    updating new files

---
+++ src/javaDSAPrac/ArraysBasics/removeDuplicateSortedArray.java
@@ -0,0 +1,23 @@
+package javaDSAPrac.ArraysBasics;
+
+public class removeDuplicateSortedArray {
+    public static int removeDuplicates(int a[]) {
+
+        int j = 0;
+        for(int i = 1; i < a.length; i++) {
+            if(a[i] != a[j]) {
+                a[j+1] = a[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+
+        int a[] = {2, 2, 3, 4, 4, 7, 7, 7};
+
+        int index = removeDuplicates(a);
+        System.out.println(index);
+    }
+}
