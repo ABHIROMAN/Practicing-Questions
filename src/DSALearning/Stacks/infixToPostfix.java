10ac4b1eb9321e22d2027ad8591dfb04a8d20359
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2023-07-13 02:09:27 +0530

    updating today

--- src/DSALearning/Stacks/infixToPostfix.java
+++ src/DSALearning/Stacks/infixToPostfix.java
@@ -12,6 +12,21 @@ public class infixToPostfix {
         else return -1;
     }
 
+    static int calculate(int a, int b, char c) {
+
+        switch(c) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return 0;
+    }
+
     static String infixToPostfixConversion(String s) {
 
         StringBuilder sb = new StringBuilder();
@@ -44,11 +59,32 @@ public class infixToPostfix {
         return sb.toString();
     }
 
+    static int evaluatePostfix(String s) {
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if(isOperator(cur)) {
+                int second = stack.pop();
+                int first = stack.pop();
+                int eval = calculate(first, second, cur);
+                stack.push(eval);
+            }
+            else {
+                stack.push(cur - '0');
+            }
+        }
+        return stack.pop();
+    }
+
     public static void main(String[] args) {
 
         String input = "(2*3)-(4/5-6+7)";
         String output = infixToPostfixConversion(input);
 
         System.out.println(output);
+
+        int evaluate = evaluatePostfix(output);
+        System.out.println(evaluate);
     }
 }
